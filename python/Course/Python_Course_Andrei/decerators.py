2e5a84e778779b15559d3c11f30cad025c92e987
Rajeev721 <rajeev.suravajhula@gmail.com>
2023-07-06 21:57:19 -0500

    commit

--- python/Course/Python_Course_Andrei/decerators.py
+++ python/Course/Python_Course_Andrei/decerators.py
@@ -1,13 +1,13 @@
 # Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
 user1 = {
     'name': 'Sorna',
-    'valid': True #changing this will either run or not run the message_friends function.
+    'valid': False #changing this will either run or not run the message_friends function.
 }
 
 def authenticated(fn):
 	def wrapper(*args, **kwargs):
-		# print(user1.get('valid'))
-		# user_dict = *args
+		print(user1.get('valid'))
+		user_dict = args
 		if args[0]['valid'] == True:
 			return fn(*args, **kwargs)
 	return wrapper
